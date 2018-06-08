6319d0d43bc425a78c77a191a8599a578dd80609
Chernyshov <xpom55@mail.ru>
2018-06-08 23:15:15 +0300

    init Vertex class, Node class and Edge class

--- kruskal.py
+++ kruskal.py
@@ -12,17 +12,19 @@ class Node(object): # disjoint set ???
     self.nodeId = nodeId
     self.parentNode = parentNode
 
-class Edge(object):
+class Edge(object): # грань, сравнение и
 
   def __init__(self, weight, startVertex, targetVertex):
     self.weight = weight
     self.startVertex = startVertex
     self.targetVertex = targetVertex
 
+  # перегрузка оператора сравнения
   def __cmp__(self, otherEdge): # сравнивать грани нужно по весам!!!
     return self.cmp(self.weight, self.weight)
 
-  def __lt__(self, other):
+  # перегрузка оператора меньше <
+  def __lt__(self, other): # less then <
     selfPriority = self.weight
     otherPriority = other.weight
     return selfPriority < otherPriority
